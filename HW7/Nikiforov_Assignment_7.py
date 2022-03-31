5fa273991f4c2a980a5618025a05c79120c2ab7a
Maksim Nikiforov <maksim.ast@gmail.com>
2022-03-31 15:46:39 -0400

    Continuing HW 7

--- HW7/Nikiforov_Assignment_7.py
+++ HW7/Nikiforov_Assignment_7.py
@@ -86,3 +86,53 @@ spark.sql("SELECT * FROM myquery_sliding") \
         .format("json") \
         .option("header", "false") \
         .save(r"C:\Users\mnikiforov\Documents\GitHub\ST590_Analysis_of_Big_Data\HW7\myquery_sliding_results")
+
+####################################
+############# Task 3 ###############
+####################################
+
+##### Setup for creating files #####
+
+import pandas as ps
+import time
+
+# Read in the all_accelerometer_data_pids_13.csv file
+accelerometer_data = ps.read_csv(r'C:\Users\mnikiforov\Documents\GitHub\ST590_Analysis_of_Big_Data\HW7\all_accelerometer_data_pids_13.csv')
+
+# Create two data frames, one for person SA0297’s data and one for person PC6771’s data.
+SA0297_data = accelerometer_data[accelerometer_data.pid == "SA0297"]
+PC6771_data = accelerometer_data[accelerometer_data.pid == "PC6771"]
+
+
+# Set up a for loop to write 500 values at a time (not randomly, from first line) for 
+# SA0297 to a .csv file in a folder for that person’s data.
+# Begin loop at first row, stop when we reach the end of our CSV file,
+# increment by 500 for slicing purposes
+for i in range(0, len(SA0297_data), 500):
+    # Starting index for slicing using .iloc[]
+    start_index = i
+    # End index (increment of 500)
+    end_index = i + 500
+    # Slice 500 rows at a time, sequentially
+    temp = file_name.iloc[start_index:end_index]
+    # Write to a .CSV file (uniquely named)
+    temp.to_csv("SA0297_data/SA0297_" \
+    + str(i) + ".csv", index = False, header = False)
+# The loop should then delay for 10 seconds after writing to the files.
+time.sleep(10)
+
+#  Similarly output values for PC6771 to another folder.
+# Begin loop at first row, stop when we reach the end of our CSV file,
+# increment by 500 for slicing purposes
+for i in range(0, len(PC6771_data), 500):
+    # Starting index for slicing using .iloc[]
+    start_index = i
+    # End index (increment of 500)
+    end_index = i + 500
+    # Slice 500 rows at a time, sequentially
+    temp = PC6771_data.iloc[start_index:end_index]
+    # Write to a .CSV file (uniquely named)
+    temp.to_csv("PC6771_data/PC6771_" \
+    + str(i) + ".csv", index = False, header = False)
+# The loop should then delay for 10 seconds after writing to the files.
+time.sleep(10)
