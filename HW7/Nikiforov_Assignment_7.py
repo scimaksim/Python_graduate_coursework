40da72297b0252882f8271f5ef87a1ada11650fa
Maksim Nikiforov <maksim.ast@gmail.com>
2022-04-02 16:22:43 -0400

    Finalized HW7

--- HW7/Nikiforov_Assignment_7.py
+++ HW7/Nikiforov_Assignment_7.py
@@ -8,28 +8,26 @@
 ####################################### Task 1 ########################################
 #######################################################################################
 
-######### Reading a stream #########
-# Generate data using the rate source
+######### Task 1 - Reading a stream #########
 
-# Import necessary libraries and initiate Spark session
-import pandas as pd
+# Create an input stream using the "rate" format at a rate of 1 row per second.
+# This outputs values with a timestamp. 
+# The values start at 0 and increase by 1 each time.
 
-# Create an input stream from the rate format using 1 row per second
 input_stream = spark \
           .readStream \
           .format("rate") \
           .option("rowsPerSecond", 1) \
           .load()
 
-######### Transform/aggregation step #########
+######### Task 1 - Transform/aggregation step #########
 
 # Import the window() function
 from pyspark.sql.functions import window
 
-# Add a watermark that relies on the timestamp column 
-# and uses a five second watermark.
-# Use it with .groupBy() to create windows that
-# are 30 seconds long with no overlap (i.e. tumbling windows).
+# Add a watermark that relies on the "timestamp" column and uses a five second watermark.
+# Use it with .groupBy() to create windows that are 
+# 30 seconds long with no overlap (i.e. tumbling windows).
 # Sum the values within that window using the .sum() aggregation
 
 windowed_stream = input_stream \
@@ -50,7 +48,7 @@ myquery = windowed_stream \
             .start()
 
 # Code to view output in the pyspark console
-spark.sql("SELECT * FROM myquery") \
+spark.sql("SELECT * FROM myquery_tumbling") \
         .coalesce(1) \
         .write \
         .format("json") \
@@ -90,6 +88,8 @@ spark.sql("SELECT * FROM myquery_sliding") \
 ####################################### Task 3 ########################################
 #######################################################################################
 
+################ Setup for crating files ###############
+
 # Import required libraries for importing data (pandas),
 # invoking 10-second delay after writing to files (time),
 # and creating directories when they do not yet exist (os)
@@ -110,13 +110,15 @@ PC6771_df = accelerometer_data[accelerometer_data.pid == "PC6771"]
 SA0297_df.name = "SA0297_df"
 PC6771_df.name = "PC6771_df"
 
+# Since .to_csv() does not create directories for us,
+# manually create new directories to store .CSV files.
+# Reference the custom ".name" attribute we created earlier
+os.makedirs('HW7/SA0297_df_sliced', exist_ok=True)
+os.makedirs('HW7/PC6771_df_sliced', exist_ok=True)
+
 # Set up a for loop to write 500 values at a time to .CSV files for PID
 # SA0297 and PC6771.
 for df_name in [SA0297_df, PC6771_df]:
-    # Since .to_csv() does not create directories for us,
-    # manually create new directories to store .CSV files.
-    # Reference the custom ".name" attribute we created earlier
-    os.makedirs('HW7/' + df_name.name + "_sliced", exist_ok=True)
     # Begin loop at first row, increment by 500 for slicing,
     # stop when we reach the last record for each PID
     for i in range(0, len(df_name), 500):
@@ -128,11 +130,11 @@ for df_name in [SA0297_df, PC6771_df]:
         temp = df_name.iloc[start_index:end_index]
         # Write to a .CSV file (uniquely named) within the new directory
         temp.to_csv("HW7/" + df_name.name + "_sliced/" + df_name.name + "_" + str(i) + ".csv", index = False, header = False)
-
 # The loop should then delay for 10 seconds after writing to the files.
 time.sleep(10)
 
-################### Reading a stream ##################
+################### Reading a stream ####################
+
 # Import StructType to help us set up schema
 from pyspark.sql.types import StructType
 
@@ -179,7 +181,8 @@ transform_PC6771 = PC6771_input_stream \
                       .alias("mag"))
 
 
-##### Writing the stream #####
+############################# Writing the stream ##############################
+
 # Write out each stream out to its own .CSV file(s).
 
 # Create new directories to store .CSV files
@@ -201,6 +204,8 @@ PC6771_stream = transform_PC6771 \
                    .option("checkpointlocation", "HW7/PC6771_df_stream_write/checkpoint") \
                    .start()
 
+####################### Combine .CSV files into single_csv_file ###################
+
 # Use PySpark to read in all "part" files
 for df_name in [SA0297_df, PC6771_df]:
     allfiles = spark.read.option("header","false").csv("HW7/" + df_name.name + "_stream_write/" + "part-*.csv")
@@ -210,3 +215,8 @@ for df_name in [SA0297_df, PC6771_df]:
     .write.format("csv") \
     .option("header", "false") \
     .save("HW7/" + df_name.name + "_stream_write/single_csv_file")
+    
+    
+#######################################################################################
+################################## End of submission ##################################
+#######################################################################################
